077de3f54fc4d73534dff2810c85ece3aa9cba13
sukipop <slsean@pm.me>
2024-05-05 03:42:15 -0600

    updated forge.sh

--- forge.sh
+++ forge.sh
@@ -38,9 +38,15 @@ while read -r pkg; do
 done < "packages.txt"
 
 # Set up zsh
+cp "assets/etc/zshrc" "$PREFIX/etc/zshrc"
 mkdir -p "$HOME/.config"
+if ! git clone git@github.com:sukipop/zsh-config.git "$HOME/.config/zsh"; then
+    if ! git clone https://github.com/sukipop/zsh-config "$HOME/.config/zsh"; then
+         error "Failed to clone zsh-config"
+    fi
+fi
 
 # Enable zsh
-if [[ "$SHELL" != "/data/data/com.termux/files/usr/bin/zsh" ]]; then
+if [[ "$SHELL" != "$PREFIX/bin/zsh" ]]; then
     chsh -s zsh
 fi
